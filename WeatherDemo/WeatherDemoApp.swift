4207909c388d2cbd12a42fe1b5b2e66cc5aa2cfe
juanpumpkimpie <pumpkinpiejuan@gmail.com>
2024-09-22 13:40:10 +0200

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Juan Guerrero on 22/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
