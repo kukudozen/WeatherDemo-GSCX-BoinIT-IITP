a29eabeec97f1e45f6fc98f89bd6fcb5fce3b14a
krakowdeveloper <pumpkinpiejuan@gmail.com>
2024-09-22 18:23:39 +0200

    images folder

--- WeatherDemo/views/WellcomeView.swift
+++ WeatherDemo/views/WellcomeView.swift
@@ -6,10 +6,30 @@
 //
 
 import SwiftUI
+import CoreLocationUI
 
 struct WellcomeView: View {
+    
+    @EnvironmentObject var locationManager: LocationManager
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            VStack (spacing: 20) {
+                Text("Welcome to the weather App")
+                    .bold().font(.title)
+                Text("Please share your current location to get the weather in your area").padding()
+            }
+            .multilineTextAlignment(.center)
+        }
+        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity)
+        .padding()
+        
+        LocationButton(.shareCurrentLocation){
+            locationManager.requestLocation()
+        }
+        .cornerRadius(30)
+        .symbolVariant(.fill)
+        .foregroundColor(.white)
     }
 }
 
