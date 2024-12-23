1a32ab1753d764d4a3527c337b873a30fad8fe7d
정선기 <jsk1610@kyonggi.ac.kr>
2024-12-23 16:27:48 +0900

    Add GSCX Scanner initialization to WeatherDemoApp

--- WeatherDemo/WeatherDemoApp.swift
+++ WeatherDemo/WeatherDemoApp.swift
@@ -1,3 +1,23 @@
+// //
+// //  WeatherDemoApp.swift
+// //  WeatherDemo
+// //
+// //  Created by Juan Guerrero on 22/09/2024.
+// //
+
+// import SwiftUI
+
+// @main
+// struct WeatherDemoApp: App {
+//     var body: some Scene {
+//         WindowGroup {
+//             ContentView()
+//         }
+//     }
+// }
+
+
+
 //
 //  WeatherDemoApp.swift
 //  WeatherDemo
@@ -6,9 +26,15 @@
 //
 
 import SwiftUI
+import GSCXScanner
 
 @main
 struct WeatherDemoApp: App {
+    init() {
+        // Initialize GSCX Scanner
+        GSCXScanner.shared().startScanning()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
